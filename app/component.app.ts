261e55d5566d0717629adec2cd32cfb9a84aefba
NatholBMX <snake-lizard@freenet.de>
2019-12-19 16:45:57 +0100

    Part 2 done

--- app/component.app.ts
+++ app/component.app.ts
@@ -2,7 +2,44 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: '<h1>Artist Directory</h1>'
+    templateUrl: './partials/app.html',
+    styleUrls: ["./css/app.css"]
 })
 
-export class AppComponent { }
+export class AppComponent {
+    name: string;
+    artists: any;
+
+    onClick(myItem, myElement) {
+        this.name = myItem.name;
+        myElement.style.backgroundColor = "#FECE4E";
+
+    }
+
+    addArtist(value) {
+        if (value !== '') {
+            this.artists.push({
+                name: value,
+                school: 'Hard Knocks'
+            });
+        }
+    }
+
+    constructor() {
+        this.name = 'Test';
+        this.artists = [
+            {
+                name: 'Barot Bellingham',
+                school: 'Penn State'
+            }, {
+                name: 'Jonathan Ferrar',
+                school: 'University of Illinios',
+            }, {
+                name: 'Hillary Post',
+                school: 'University of Florida'
+            }
+
+
+        ]
+    }
+}
